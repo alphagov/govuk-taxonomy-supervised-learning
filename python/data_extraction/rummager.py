7d733295f309f2f21a44d9264d72af5cc3974eef
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2018-01-23 14:03:21 +0000

    Add api for Rummager
    The paged rummager REST api is accessed by a generator.

---
+++ python/data_extraction/rummager.py
@@ -0,0 +1,25 @@
+from itertools import count
+from data_extraction.helpers import merge
+from data_extraction import plek
+import requests
+
+
+class Rummager():
+
+    def __init__(self, base_url=plek.find("rummager")):
+        self.base_url = base_url
+
+    def search_generator(self, args, page_size=100):
+        for index in count(page_size):
+            search_params = merge(args, {"start": index, "count": page_size})
+            results = self.__search(search_params).get('results', [])
+            for result in results:
+                yield result
+            if len(results) < page_size:
+                break
+
+    # PRIVATE
+
+    def __search(self, args):
+        request_url = "{base_url}/search.json".format(base_url=self.base_url)
+        return requests.get(request_url, args).json()
