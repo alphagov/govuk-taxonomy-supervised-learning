46b0612f7849988d4a3bf37178e4d23f49314d06
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2018-01-23 16:26:46 +0000

    Add method to get content from content store from content link

--- python/data_extraction/content_export.py
+++ python/data_extraction/content_export.py
@@ -7,3 +7,41 @@ def content_links_generator(page_size=1000, rummager_url=plek.find('rummager'), 
     search_results = rummager.Rummager(rummager_url).search_generator(
         {'reject_content_store_document_type': blacklist_document_types, 'fields': ['link']}, page_size=page_size)
     return map(lambda h: h.get('link'), search_results)
+
+
+def get_content(base_path,
+                base_fields=[],
+                taxon_fields=[],
+                ppo_fields=[],
+                content_store_url=plek.find('draft-content-store')):
+    content_hash = __get_content_hash(base_path, content_store_url)
+
+    # Skip this if we don't get back the content we expect, e.g. if
+    # the Content Store has redirected the request
+    if content_hash.get('base_path') != base_path:
+        return {}
+
+    # Skip anything without a content_id
+    if 'content_id' not in content_hash:
+        return {}
+
+    base = slice(content_hash, base_fields)
+    taxons = dig(content_hash, 'links', 'taxons')
+    ppo = dig(content_hash, 'links', 'primary_publishing_organisation')
+
+    if taxons:
+        base['taxons'] = [slice(taxon, taxon_fields) for taxon in taxons]
+    if ppo:
+        base['primary_publishing_organisation'] = slice(ppo[0], ppo_fields)
+
+    return base
+
+
+# PRIVATE
+def __get_content_hash(path, content_store_url):
+    url = "{content_store_url}/content{path}".format(content_store_url=content_store_url, path=path)
+    response = requests.get(url)
+    if response.status_code == 200:
+        return requests.get(url).json()
+    else:
+        return {}
--- python/test/data_extraction/test_content_export.py
+++ python/test/data_extraction/test_content_export.py
@@ -3,6 +3,38 @@ from data_extraction import content_export
 import responses
 
 
+def content_with_taxons():
+    return {
+        "base_path": "/base_path",
+        "content_id": "d282d35a-2bd2-4e14-a7a6-a04e6b10520f",
+        "links": {
+            "taxons": [{"content_id": "237b2e72-c465-42fe-9293-8b6af21713c0"},
+                       {"content_id": "8da62d85-47c0-42df-94c4-eaaeac329671"}]
+        }
+    }
+
+
+def content_with_ppo():
+    return {
+        "base_path": "/base_path",
+        "content_id": "d282d35a-2bd2-4e14-a7a6-a04e6b10520f",
+        "links": {
+            "primary_publishing_organisation": [
+                {"title": "title1"}
+            ]
+        },
+    }
+
+
+def content_no_taxon():
+    return {
+        "base_path": "/base_path",
+        "content_id": "d282d35a-2bd2-4e14-a7a6-a04e6b10520f",
+        "links": {
+        }
+    }
+
+
 def content_links():
     return {
         'results': [{'link': '/first/path'}, {'link': '/second/path'}]
@@ -16,3 +48,37 @@ class ContentLinksGenerator(unittest.TestCase):
         responses.add(responses.GET, "http://example.com/search.json", json=content_links())
         response = content_export.content_links_generator(rummager_url="http://example.com")
         self.assertListEqual(list(response), ['/first/path', '/second/path'])
+
+
+class GetContent(unittest.TestCase):
+
+    @responses.activate
+    def test_empty_hash(self):
+        responses.add(responses.GET, "http://example.com/content/base_path", status=404)
+        self.assertDictEqual(content_export.get_content('/base_path', content_store_url="http://example.com"), {})
+
+    @responses.activate
+    def test_simple_content(self):
+        responses.add(responses.GET, "http://example.com/content/base_path", json=content_no_taxon())
+        expected = {"base_path": "/base_path", "content_id": "d282d35a-2bd2-4e14-a7a6-a04e6b10520f"}
+        response = content_export.get_content('/base_path',
+                                             base_fields = ["base_path", "content_id"],
+                                             content_store_url="http://example.com")
+        self.assertDictEqual(response, expected)
+
+    @responses.activate
+    def test_taxons(self):
+        responses.add(responses.GET, "http://example.com/content/base_path", json=content_with_taxons())
+        expected = [{"content_id": "237b2e72-c465-42fe-9293-8b6af21713c0"},
+                    {"content_id": "8da62d85-47c0-42df-94c4-eaaeac329671"}]
+        response = content_export.get_content('/base_path', content_store_url="http://example.com",
+                                              taxon_fields=["content_id"])
+        self.assertListEqual(response.get('taxons'), expected)
+
+    @responses.activate
+    def test_primary_publishing_organisations(self):
+        responses.add(responses.GET, "http://example.com/content/base_path", json=content_with_ppo())
+        expected = {"title": "title1"}
+        response = content_export.get_content('/base_path', content_store_url="http://example.com",
+                                              ppo_fields=["title"])
+        self.assertDictEqual(response.get('primary_publishing_organisation'), expected)
