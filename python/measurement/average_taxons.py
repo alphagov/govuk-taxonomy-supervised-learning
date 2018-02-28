e2177fe1dd2e765305fd127f297f7ab32284dfe0
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-02-28 17:23:49 +0000

    Add a progress bar to measure_average_taxons

--- python/measurement/average_taxons.py
+++ python/measurement/average_taxons.py
@@ -1,5 +1,6 @@
 import gzip
 import ijson
+import progressbar
 from lib import json_arrays
 from lib.helpers import dig
 from lib import services
@@ -15,6 +16,8 @@ def measure_average_taxons(filename):
         taxons = dig(content_item, 'links', 'taxons') or []
         return len([taxon for taxon in taxons if __query.taxon_linked_to_root(taxon)])
 
+    progress_bar = progressbar.ProgressBar()
+
     with gzip.open(filename, mode='rt') as file:
         content_items = ijson.items(file, prefix='item')
 
@@ -24,8 +27,9 @@ def measure_average_taxons(filename):
                 # taxonomy, as removing this gives a more useful
                 # measure of what the makeup of the tagged content is
                 lambda count: count != 0,
-                map(__number_of_taxons, content_items)
+                map(__number_of_taxons, progress_bar(content_items))
             )
         )
 
+        print("Found an average of {} taxons per content item".format(value))
         services.statsd.gauge('average_taxons_per_content_item', value)
