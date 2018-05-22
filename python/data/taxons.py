4e8af9f6923d1d20d67698003b9e6bf711aea3bb
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-05-22 10:25:29 +0000

    Add a function to filter content by part of taxonomy

--- python/data/taxons.py
+++ python/data/taxons.py
@@ -27,3 +27,11 @@ def content_item_tagged_to_topic_taxonomy(content_item):
             content_item_taxons(content_item)
         )
     )
+
+def content_item_within_part_of_taxonomy(content_item, taxonomy_part_content_id):
+    return any(
+        filter(
+            lambda taxon: taxon["content_id"] == taxonomy_part_content_id,
+            content_item_taxons(content_item)
+        )
+    )
