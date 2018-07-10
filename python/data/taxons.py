b46a9ce6ee3da5123074f26dacc0f900fbebf156
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-07-10 13:40:03 +0100

    Add content_item_directly_tagged_to_taxon
    To the taxons module. This allows for checking if a content item is
    directly tagged to a taxon (by content_id).

--- python/data/taxons.py
+++ python/data/taxons.py
@@ -35,3 +35,11 @@ def content_item_within_part_of_taxonomy(content_item, taxonomy_part_content_id)
             content_item_taxons(content_item)
         )
     )
+
+def content_item_directly_tagged_to_taxon(content_item, taxon_content_id):
+    return any(
+        map(
+            lambda taxon: taxon["content_id"] == taxon_content_id,
+            content_item["links"].get("taxons", [])
+        )
+    )
